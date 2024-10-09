6252bcbc45eb4403cf50897e788b63726df4b441
Andrés Correa <andres.correar@est.iudigital.edu.co>
2024-10-09 11:26:57 -0500

    Agregar axios-config.js a la carpeta helper

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
    // baseURL: 'http://localhost:4000/'
-    baseURL: 'https://cinema-2-lz6m.onrender.com/'
+    baseURL: 'https://cinema-2-hz5g.onrender.com'
 });
 
 export {
